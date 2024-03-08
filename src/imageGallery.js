08ab478ab547abea97ff4f15c8d793fbd514756d
onedionys <onedionys@gmail.com>
2024-03-08 10:53:50 +0700

    chore: initialize function project

---
+++ src/imageGallery.js
@@ -0,0 +1,14 @@
+class ImageGallery {
+    constructor(images) {
+        this.images = images;
+    }
+
+    // Method to initialize and render the image gallery
+    renderGallery() {
+        // Implementation logic to render the image gallery
+    }
+
+    // Other methods for interacting with the gallery (e.g., addImage, removeImage, etc.)
+}
+
+module.exports = ImageGallery;
